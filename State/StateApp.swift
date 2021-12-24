a8dca724500c9556d3e84ccb135233b800a7a139
Ivan Dudarev <dudarevmail@icloud.com>
2021-12-24 22:56:43 +0500

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Ivan Dudarev on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
